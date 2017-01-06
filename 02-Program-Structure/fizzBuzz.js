09a689dcb067dccd1c4d96fe58b3b55aad69dcc7
João Paulo Effting <joaoeffting@gmail.com>
2017-01-06 14:44:36 -0200

    Eloquente JavaScript - 02 - Data Structure - FizzBuzz exercise.

--- 02-Program-Structure/fizzBuzz.js
+++ 02-Program-Structure/fizzBuzz.js
@@ -1,1 +1,28 @@
-//TODO
+//Check if a number is divisible by three
+function isDivisibleByThree(number) {
+  return number % 3 == 0;
+}
+
+//Check if a number is divisible by five
+function isDivisibleByFive(number) {
+  return number % 5 == 0;
+}
+
+//Check if a number is divisible by three and five
+function isDivisibleByThreeAndFive(number) {
+  return isDivisibleByThree(number) && isDivisibleByFive(number);
+}
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (isDivisibleByThreeAndFive(i)) {
+      console.log('FizzBuzz');
+    } else if (isDivisibleByThree(i)) {
+      console.log('Fizz');
+    } else if (isDivisibleByFive(i)) {
+      console.log('Buzz');
+    }
+  }
+}
+
+fizzBuzz();
