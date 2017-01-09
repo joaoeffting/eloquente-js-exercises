d5d0cf3a09dbcca1ad49fe85fd32959af0ac2115
João Paulo Effting <joaoeffting@gmail.com>
2017-01-09 12:50:33 -0200

    Resolving the 3 - Chess board exercice

--- 02-Program-Structure/chessBoard.js
+++ 02-Program-Structure/chessBoard.js
@@ -1,1 +1,12 @@
-//TODO
+function chessBoard() {
+  let chessBoard = '# # # # # # # #';
+    for (let i = 1; i<= 8; i++) {
+      if (i % 2 == 0) {
+        console.log(chessBoard);
+      } else {
+        console.log(' '+chessBoard);
+      }
+    }
+}
+
+chessBoard();
